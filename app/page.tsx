b3669bb8df95265a1c4bce2a04b8f77b7b0f35ca
Khunnie <chongchangrun@gmail.com>
2026-05-21 12:00:32 +0700

    change homepage

--- app/page.tsx
+++ app/page.tsx
@@ -2,17 +2,19 @@
 
 export default function Home() {
   return (
-    <div style={{
-      minHeight: "100vh",
-      display: "flex",
-      justifyContent: "center",
-      alignItems: "center",
-      background: "#0f172a",
-      color: "white",
-      fontSize: "40px",
-      fontWeight: "bold"
-    }}>
-      EduKinder ทำงานแล้ว 🎉
+    <div
+      style={{
+        minHeight: "100vh",
+        background: "#0f172a",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        color: "white",
+        fontSize: "50px",
+        fontWeight: "bold",
+      }}
+    >
+      EduKinder 🎉
     </div>
   );
 }
